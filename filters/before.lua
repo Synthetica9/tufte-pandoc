689ee2167e590493c6d225a00f0a4b0249120c50
Patrick Hilhorst <git@hilhorst.be>
2019-12-17 16:07:05 +0100

    Make sure raw output works as intended

--- filters/before.lua
+++ filters/before.lua
@@ -122,3 +122,11 @@ function Div(div)
     end
   end
 end
+
+function RawBlock(block)
+  if block.format == "markdown" then
+    print('Extracting RawBlock')
+    x = pandoc.read(block.text, "markdown")
+    return pandoc.Div(x.blocks)
+  end
+end
--- md-src/01_chap_1.md
+++ md-src/01_chap_1.md
@@ -4,3 +4,7 @@
 # New file new session!
 print(__file__)
 ```
+
+```{ .bash .cb.run show=code+stdout:raw }
+echo '`.cb.run` can include *inline* Markdown'
+```
