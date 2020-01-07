2bcaed049276d8b9b7bad3c07cc8c6b537091705
Patrick Hilhorst <git@hilhorst.be>
2020-01-07 19:20:04 +0100

    Open meta.yaml after init script runs

--- init.sh
+++ init.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+# Remove cruft:
 rm -v LICENSE
 rm -v README.md
 rm -v md-src/0*.md
 rm -v bibliography.bib && touch bibliography.bib
 rm -v init.sh
 rm -v environments/*
+
+# Open the the metadata in your favourite editor:
+exec $EDITOR meta.yaml
