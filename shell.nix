960474afe0cf39f76e5dacd8d571433d46340704
Patrick Hilhorst <git@hilhorst.be>
2020-02-07 18:07:28 +0100

    Update shell.nix to use latest unstable

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ pkgsHash ? "86ed15dcce7de9c9cac5755663b622142a89d76d" }:
+{ pkgsHash ? "0c960262d159d3a884dadc3d4e4b131557dad116" }:
 
 let
   nixpkgs = builtins.fetchTarball "https://github.com/nixos/Nixpkgs/archive/${pkgsHash}.tar.gz";
