4bf40d8992df9ca317182617daef403556054048
Patrick Hilhorst <git@hilhorst.be>
2020-07-07 17:45:02 +0200

    Update shell.nix hashes

--- shell.nix
+++ shell.nix
@@ -1,8 +1,8 @@
-{ pkgsHash ? "d363be93b4957fce418a00340edc8b9bad5a1c88" }:
+{ pkgsHash ? "codebraid-2020-07-01" }:
 
 let
   nixpkgs = builtins.fetchTarball
-    "https://github.com/nixos/Nixpkgs/archive/${pkgsHash}.tar.gz";
+    "https://github.com/synthetica9/Nixpkgs/archive/${pkgsHash}.tar.gz";
   pkgs = import nixpkgs { config.allowBroken = true; };
 
 in with pkgs;
@@ -13,17 +13,13 @@ let
       scheme-medium
       # Required packages:
       ifoddpage relsize xifthen ifmtarg datatool xfor substr trimspaces cleveref
-      tufte-latex hardwrap titlesec semantic
+      tufte-latex hardwrap titlesec semantic catchfile
 
       # User packages:
       lipsum;
   };
 
-  ourHaskellPackages = haskell.packages.ghc881;
-  ourPandoc = ourHaskellPackages.pandoc_2_9_1_1.overrideAttrs (old: {
-    # 2020-02-25: currently pandoc tests are failing.
-    doCheck = false;
-  });
+  ourHaskellPackages = haskell.packages.ghc883;
 
   scons_py_packages = python38Packages;
   scons_py3 = scons.override { python2Packages = scons_py_packages; };
@@ -34,12 +30,13 @@ let
 in mkShell {
   buildInputs = with ourHaskellPackages; [
     ourLatex
-    ourPandoc
     ourScons
 
+	pandoc
     codebraid
     librsvg
-    pandoc-citeproc_0_16_4_1
+    pandoc-citeproc_0_17_0_1
+    entr
   ];
 
   NIX_PATH = "nixpkgs=${nixpkgs}";
